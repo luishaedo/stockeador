6583f0163db178a49c49beb59ace509ccbc760e3
luishaedo <luismariahaedolanda@gmail.com>
2025-03-18 18:26:03 -0300

    limpiando para deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: './client',  // Asegúrate de que esté configurado correctamente si estás trabajando en una subcarpeta
   build: {
-    outDir: 'dist',
-  },
-  publicDir: 'public',  // Verifica que esto esté configurado correctamente
+    outDir: 'dist',  
+  }
 });
-
